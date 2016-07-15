f25ecffbdd9de68b2b22f396f335f7fac3fbba55
lukasmartinelli <me@lukasmartinelli.ch>
2016-07-15 22:25:53 +0200

    Refine admin

--- src/db-schema/layers/admin.sql
+++ src/db-schema/layers/admin.sql
@@ -13,27 +13,38 @@ $$ LANGUAGE plpgsql IMMUTABLE;
 
 CREATE OR REPLACE VIEW admin_z2 AS (
     SELECT geom,
-           0 AS admin_level,
+           0 AS admin_level, scalerank,
            admin_class(featurecla) AS class
     FROM ne_110m_admin_0_boundary_lines_land
 );
 
 CREATE OR REPLACE VIEW admin_z3 AS (
-    SELECT geom, 0 AS admin_level,
+    SELECT geom, 0 AS admin_level, scalerank,
            admin_class(featurecla) AS class
     FROM ne_50m_admin_0_boundary_lines_land
     UNION ALL
-    SELECT geom, 1 AS admin_level,
+    SELECT geom, 1 AS admin_level, scalerank,
            admin_class(featurecla) AS class
     FROM ne_50m_admin_1_states_provinces_lines
 );
 
 CREATE OR REPLACE VIEW admin_z5 AS (
-    SELECT geom, 0 AS admin_level,
+    SELECT geom, 0 AS admin_level, scalerank,
            admin_class(featurecla) AS class
     FROM ne_10m_admin_0_boundary_lines_land
     UNION ALL
-    SELECT geom, 1 AS admin_level,
+    SELECT geom, 1 AS admin_level, scalerank,
+           admin_class(featurecla) AS class
+    FROM ne_10m_admin_1_states_provinces_lines_shp
+    WHERE scalerank <= 7
+);
+
+CREATE OR REPLACE VIEW admin_z7 AS (
+    SELECT geom, 0 AS admin_level, scalerank,
+           admin_class(featurecla) AS class
+    FROM ne_10m_admin_0_boundary_lines_land
+    UNION ALL
+    SELECT geom, 1 AS admin_level, scalerank,
            admin_class(featurecla) AS class
     FROM ne_10m_admin_1_states_provinces_lines_shp
 );
