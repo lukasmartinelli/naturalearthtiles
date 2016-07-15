da0e79c1533f0de16beb2b2355adb27da059d437
lukasmartinelli <me@lukasmartinelli.ch>
2016-07-15 22:44:42 +0200

    Remove geographic lines

--- src/db-schema/create_schema.sh
+++ src/db-schema/create_schema.sh
@@ -26,7 +26,6 @@ function main() {
     exec_psql_file "layers/ice.sql"
     exec_psql_file "layers/place_labels.sql"
     exec_psql_file "layers/port_labels.sql"
-    exec_psql_file "layers/geographic_lines.sql"
     exec_psql_file "layers/admin.sql"
     exec_psql_file "layers/airport_labels.sql"
     exec_psql_file "layers/river.sql"
