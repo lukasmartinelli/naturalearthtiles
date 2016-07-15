4435e8a439d79c6ac8309be87f1ab520ace10746
lukasmartinelli <me@lukasmartinelli.ch>
2016-07-15 19:52:07 +0200

    Allow specifying bbox

--- src/export-vectortiles/export-local.sh
+++ src/export-vectortiles/export-local.sh
@@ -8,12 +8,14 @@ readonly EXPORT_DIR="/export"
 readonly RENDER_SCHEME=${RENDER_SCHEME:-pyramid}
 readonly MIN_ZOOM=${MIN_ZOOM:-0}
 readonly MAX_ZOOM=${MAX_ZOOM:-7}
+readonly BBOX=${BBOX:-"-180 -85.0511 180 85.0511"}
 
 function export_local_mbtiles() {
     local mbtiles_name="natural_earth.mbtiles"
 
     exec tl copy \
         -s pyramid \
+        -b "$BBOX" \
         --min-zoom="$MIN_ZOOM" \
         --max-zoom="$MAX_ZOOM" \
         "tmsource://$PROJECT_DIR" "mbtiles://$EXPORT_DIR/$mbtiles_name"
