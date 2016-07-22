b761b355a1c13b1088295d852e41a373eafe9c2c
lukasmartinelli <me@lukasmartinelli.ch>
2016-07-22 21:52:06 +0200

    Improve country label quality

--- src/db-schema/layers/country_label.sql
+++ src/db-schema/layers/country_label.sql
@@ -1,17 +1,23 @@
+DROP TABLE IF EXISTS country_label CASCADE;
+CREATE TABLE country_label AS (
+    SELECT DISTINCT on (nec.adm0_a3)
+    nel.geom,
+    nec.name, nec.iso_a3, nec.abbrev, 
+    nel.scalerank, nec.labelrank
+    FROM ne_10m_admin_0_countries AS nec 
+    INNER JOIN ne_10m_admin_0_label_points AS nel
+    ON nec.adm0_a3 = nel.sr_adm0_a3 AND nel.scalerank <= 1
+);
+CREATE INDEX country_label_geom_idx ON country_label USING gist(geom);
+
 CREATE OR REPLACE VIEW country_label_z3 AS (
-    SELECT geom,
-           sr_subunit AS name,
-           sr_adm0_a3 AS abbreviation,
-           scalerank
-    FROM ne_10m_admin_0_label_points
+    SELECT *
+    FROM country_label
     WHERE scalerank = 0
 );
 
 CREATE OR REPLACE VIEW country_label_z5 AS (
-    SELECT geom,
-           sr_subunit AS name,
-           sr_adm0_a3 AS abbreviation,
-           scalerank
-    FROM ne_10m_admin_0_label_points
+    SELECT *
+    FROM country_label
     WHERE scalerank <= 1
 );
