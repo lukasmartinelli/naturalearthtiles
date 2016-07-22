ff549b81ed425a6023d3cc475fca294879339881
lukasmartinelli <me@lukasmartinelli.ch>
2016-07-22 18:01:15 +0200

    Generalize WWF data

--- src/db-schema/layers/biome.sql
+++ src/db-schema/layers/biome.sql
@@ -20,7 +20,36 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql IMMUTABLE;
 
-CREATE OR REPLACE VIEW biome_z2 AS (
-    SELECT geom, biome_type(biome::int) AS biome
+DROP TABLE IF EXISTS biome_gen0 CASCADE;
+CREATE TABLE biome_gen0 AS (
+    SELECT ST_SimplifyPreserveTopology(geom, 500) AS geom, biome_type(biome::int) AS biome
     FROM wwf_terr_ecos
+);
+CREATE INDEX biome_gen0_geom_idx ON biome_gen0 USING gist(geom);
+
+DROP TABLE IF EXISTS biome_gen1 CASCADE;
+CREATE TABLE biome_gen1 AS (
+    SELECT ST_SimplifyPreserveTopology(geom, 5000) AS geom, biome_type(biome::int) AS biome
+    FROM wwf_terr_ecos
+);
+CREATE INDEX biome_gen1_geom_idx ON biome_gen1 USING gist(geom);
+
+DROP TABLE IF EXISTS biome_gen2 CASCADE;
+CREATE TABLE biome_gen2 AS (
+    SELECT ST_SimplifyPreserveTopology(geom, 20000) AS geom, biome_type(biome::int) AS biome
+    FROM wwf_terr_ecos
+);
+CREATE INDEX biome_gen2_geom_idx ON biome_gen2 USING gist(geom);
+
+
+CREATE OR REPLACE VIEW biome_z2 AS (
+    SELECT * FROM biome_gen2
+);
+
+CREATE OR REPLACE VIEW biome_z4 AS (
+    SELECT * FROM biome_gen1
+);
+
+CREATE OR REPLACE VIEW biome_z6 AS (
+    SELECT * FROM biome_gen0
 );
