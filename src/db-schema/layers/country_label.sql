172edccb9bf60587df6b0fdb16f3a24e5f3c6c58
lukasmartinelli <me@lukasmartinelli.ch>
2016-07-23 17:26:11 +0200

    Exclude tiny countries

--- src/db-schema/layers/country_label.sql
+++ src/db-schema/layers/country_label.sql
@@ -3,14 +3,15 @@ CREATE TABLE country_label AS (
     SELECT topoint(geom) AS geom,
            name,
            adm0_a3, abbrev,
-           scalerank, labelrank
+           scalerank, labelrank,
+           CASE WHEN tiny < 0 THEN 0 ELSE 1 END AS is_tiny
     FROM ne_10m_admin_0_map_subunits
     WHERE scalerank <= 1
 );
 CREATE INDEX country_label_geom_idx ON country_label USING gist(geom);
 
 CREATE OR REPLACE VIEW country_label_z3 AS (
-    SELECT * FROM country_label WHERE scalerank = 0
+    SELECT * FROM country_label WHERE scalerank = 0 AND is_tiny = 0
 );
 
 CREATE OR REPLACE VIEW country_label_z5 AS (
