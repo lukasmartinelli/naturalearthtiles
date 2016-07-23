68e45206cf4a760c99b2bd053dca3ab74bdb0fec
lukasmartinelli <me@lukasmartinelli.ch>
2016-07-23 14:25:25 +0200

    Add more state label info

--- src/db-schema/layers/state_label.sql
+++ src/db-schema/layers/state_label.sql
@@ -1,15 +1,15 @@
 CREATE OR REPLACE VIEW state_label_z3 AS (
-    SELECT geom, name, scalerank
+    SELECT geom, name, scalerank, labelrank, postal
     FROM ne_50m_admin_1_states_provinces_shp
 );
 
 CREATE OR REPLACE VIEW state_label_z5 AS (
-    SELECT geom, name, scalerank
+    SELECT geom, name, scalerank, labelrank, postal
     FROM ne_10m_admin_1_states_provinces_shp
     WHERE scalerank <= 6
 );
 
 CREATE OR REPLACE VIEW state_label_z7 AS (
-    SELECT geom, name, scalerank
+    SELECT geom, name, scalerank, labelrank, postal
     FROM ne_10m_admin_1_states_provinces_shp
 );
