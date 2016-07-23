a38f8ad19a16476160398ca42853f1138a8b49b0
lukasmartinelli <me@lukasmartinelli.ch>
2016-07-23 16:16:51 +0200

    Fix state label text encoding

--- src/db-schema/layers/state_label.sql
+++ src/db-schema/layers/state_label.sql
@@ -1,3 +1,17 @@
+CREATE OR REPLACE FUNCTION fix_win1252_shp_encoding(str TEXT) RETURNS TEXT
+AS $$
+BEGIN
+    RETURN convert_from(convert_to(str, 'WIN1252'), 'UTF-8');
+    EXCEPTION WHEN others THEN RETURN str;
+END;
+$$ LANGUAGE plpgsql IMMUTABLE;
+
+UPDATE ne_10m_admin_1_states_provinces_shp
+SET name=fix_win1252_shp_encoding(name);
+
+UPDATE ne_50m_admin_1_states_provinces_shp
+SET name=fix_win1252_shp_encoding(name);
+
 CREATE OR REPLACE VIEW state_label_z3 AS (
     SELECT geom, name, scalerank, labelrank, postal
     FROM ne_50m_admin_1_states_provinces_shp
