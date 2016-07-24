fdd56bb4653790e59722a059ab103fb9f8d64fa9
lukasmartinelli <me@lukasmartinelli.ch>
2016-07-24 10:15:11 +0200

    Fix water vector data source

--- src/db-schema/layers/water.sql
+++ src/db-schema/layers/water.sql
@@ -1,23 +1,23 @@
 CREATE OR REPLACE VIEW water_z0 AS (
-    SELECT geom, 0 AS scalerank FROM ne_110m_ocean
+    SELECT geom FROM ne_110m_ocean
     UNION ALL
-    SELECT geom, scalerank FROM ne_110m_lakes
+    SELECT geom FROM ne_110m_lakes
 );
 
 CREATE OR REPLACE VIEW water_z1 AS (
-    SELECT geom, 0 AS scalerank FROM ne_50m_ocean
+    SELECT geom FROM ne_50m_ocean
     UNION ALL
-    SELECT geom, scalerank FROM ne_110m_lakes
+    SELECT geom FROM ne_110m_lakes
 );
 
 CREATE OR REPLACE VIEW water_z2 AS (
-    SELECT geom, 0 AS scalerank FROM ne_10m_ocean
+    SELECT geom FROM ne_10m_ocean
     UNION ALL
-    SELECT geom, scalerank FROM ne_50m_lakes
+    SELECT geom FROM ne_50m_lakes
 );
 
 CREATE OR REPLACE VIEW water_z4 AS (
-    SELECT geom, 0 AS scalerank FROM ne_10m_ocean
+    SELECT geom FROM ne_10m_ocean
     UNION ALL
-    SELECT geom, scalerank FROM ne_10m_lakes
+    SELECT geom FROM ne_10m_lakes
 );
